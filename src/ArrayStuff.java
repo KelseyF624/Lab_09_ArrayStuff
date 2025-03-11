1b7e3ed6de76f83db1cbdc251d2b47a985017dbc
KelseyF624 <kelseyf.624@gmail.com>
2025-03-11 18:06:56 -0400

    Partial Commit :)

--- src/ArrayStuff.java
+++ src/ArrayStuff.java
@@ -1,9 +1,58 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class ArrayStuff {
 
     public static void main(String[] args) {
-        
+        //task 1
+
+        int[] dataPoints = new int[100];
+
+        //task 2
+
+        Random rand = new Random();
+        for (int i = 0; i < dataPoints.length; i++) {
+            dataPoints[i] = rand.nextInt(100)+1;}
+        for (int i = 0; i < dataPoints.length; i++) {
+            System.out.print (dataPoints[i] + "  ");}
+
+        //task 3
+
+        for (int i = 0; i < dataPoints.length; i++) {
+            dataPoints[i] = rand.nextInt(100)+1;}
+        for (int i = 0; i < dataPoints.length; i++) {
+            System.out.print (dataPoints[i] + " | ");}
+
+        //task 4
+
+        int sum = 0;
+        for (int i = 0; i < dataPoints.length; i++) {
+            sum = sum + dataPoints[i];
+            System.out.print ("The sum of the data points is:" + sum);
+        }
+        int ave = sum / dataPoints.length;
+        System.out.println("\nThe average of the data points is:" + ave);
+
+        //task 5
+
+        Scanner input = new Scanner(System.in);
+        int intValue =SafeInput.getRangedInt(input, "Please enter a value between 1 and 100.", 1, 100);
+
+        //task 6
+
+
+
+        //task 7
+
+
+
+        //task 8
+
+
+
+        //task 9
+
+
 
     }
 }
